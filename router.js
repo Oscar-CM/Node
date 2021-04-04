aff6a2d4731ca1f68a4a41dd6e41c0ca1140655e
Oscar <oscarcheruiyot3@gmail.com>
2021-04-04 12:39:31 +0300

    initial commit

---
+++ router.js
@@ -0,0 +1,40 @@
+const e = require("express");
+var express = require("express");
+var router = express.Router();
+
+
+const credential = {
+    email:"oscar@gmail.com",
+    password:"admin123"
+}
+//login user
+router.post('/login',(req,res)=>{
+    if(req.body.email == credential.email && req.body.password == credential.password){
+
+        req.session.user = req.body.email;
+        res.redirect('/route/dashboard'); 
+        //res.end('Login Successfull!')
+    }else{
+        res.end("Invalid Username")
+    }
+});
+//dashboard route
+router.get('/dashboard',(req,res)=>{
+    if(req.session.user){
+        res.render('dashboard',{user:req.session.user})
+    }else{
+        req.sessionID("Unauthorized User")
+    }
+})
+ //logout route
+ router.get('/logout', (req,res) =>{
+     req.session.destroy(function(err){
+         if(err){
+             console.log(err);
+             res.send("Error")
+         }else{
+             res.render('base',{title:"Express",logout:"logout successfully"})
+         }
+     })
+ })
+module.exports = router;
